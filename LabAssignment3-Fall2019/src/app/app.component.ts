c2d98cb01962133ee887e596880459abee24435c
Jim <jimyyang129@gmail.com>
2019-12-13 18:11:11 -0800

    add header,content, and footer

---
+++ LabAssignment3-Fall2019/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'LabAssignment3-Fall2019';
+  
+
+  constructor() {
+
+  }
+}
+
+// 47 min
